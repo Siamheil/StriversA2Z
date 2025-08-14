49ca888d2108150f20052d8cd8c5c7367481aa30
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-14 22:46:17 +0530

    Add files via upload

---
+++ DataTypes.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a=2;
+	long b=2334;
+	long long x=1200000000000;
+	
+	float y=3.14;
+	double z-4.56565034;
+	
+	char c='e';
+	bool g=true;
+	
+	
+	return 0;
+}
