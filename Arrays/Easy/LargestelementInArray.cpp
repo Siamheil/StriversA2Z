1b8ef38e0f9c5a56a3bf8e54f8a48ffefa3233d0
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-15 18:37:55 +0530

    LargestelementInArray.cpp

--- Arrays/Easy/LargestelementInArray.cpp
+++ Arrays/Easy/LargestelementInArray.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        int max=INT_MIN;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+};
