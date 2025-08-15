7a25e7ac3dd6a81faab498e8e0ff6d556cc83812
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-15 12:59:13 +0530

    Add files via upload

---
+++ Basics/Basic Maths/PrintallDivisors.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printallDivisors(int n){
+	for(int i=1;i<=n;i++){
+		if(n%i==0){
+			cout<<i<<" ";
+		}
+	}
+}
+
+int main(){
+	int n;
+	cin>>n;
+	printallDivisors(n);
+	return 0;
+	
+}
