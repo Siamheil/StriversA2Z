953a493eb7742a82359eebbc5767441b9fb1b228
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-15 08:58:05 +0530

    CountDigits.cpp

--- Basics/Basic Maths/CountDigits.cpp
+++ Basics/Basic Maths/CountDigits.cpp
@@ -1,1 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int count_digits(int n){
+	int count=0;
+	while(n){
+		count++;
+		n=n/10;
+	}
+	return count;
+}
+int main(){
+	int n;
+	cin>>n;
+	cout<<count_digits(n);
+	return 0;
+}
