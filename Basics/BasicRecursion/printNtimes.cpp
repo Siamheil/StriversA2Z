09ab6f8cb0c7c703774de4272816a5de98f90e5e
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-15 14:29:16 +0530

    printNtimes.cpp

--- Basics/BasicRecursion/printNtimes.cpp
+++ Basics/BasicRecursion/printNtimes.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void printNtimes(int n){
+	if(n==0){
+		return;
+	}
+	cout<<"hello "<<endl;
+	printNtimes(n-1);
+}
+
+int main(){
+	int n;
+	cin>>n;
+	printNtimes(n);
+	
+	
+	return 0;
+}
