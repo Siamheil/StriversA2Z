35806804f186e5f17b838d813d806d0f48dd3dc6
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-17 13:43:18 +0530

    Update RotatebyKplaces.cpp

--- Arrays/Easy/RotatebyKplaces.cpp
+++ Arrays/Easy/RotatebyKplaces.cpp
@@ -1,1 +1,22 @@
+void Reverse(vector<int>&nums,int start,int end){
+    while(start<=end){
+        int temp=nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+        start++;
+        end--;
+    }
+}
 
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        Reverse(nums,0,n-1);
+        Reverse(nums,0,k-1);
+        Reverse(nums,k,n-1);
+
+    }
+};
