ba714e2f0ef0004a7a891b524fce9b2bdac5db68
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-24 09:51:36 +0530

    Create TwoSum.cpp

---
+++ Arrays/Medium/TwoSum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        int start=0,end=n-1;
+        while(start<end){
+            int sum=arr[start]+arr[end];
+            if(sum==target){
+                return true;
+            }else if(sum<target){
+                start++;
+            }else{
+                end--;
+            }
+        }
+        return false;
+    }
+};
